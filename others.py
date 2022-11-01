d336262b40c2b51187bcda6187acaa08e2083cd6
olusK <kozak.aleksandra96@gmail.com>
2022-11-01 19:13:27 +0100

    calendar functions

--- others.py
+++ others.py
@@ -11,3 +11,20 @@ def get_calendar_card():
     Calendar = calendar.TextCalendar()
     calendar_card = Calendar.formatmonth(current_year, current_month)
     print(calendar_card)
+
+'''Write a Python program to calculate number of days between two dates.'''
+
+def get_day_distance():
+    t1 = input('Podaj pierwszą datę w formacie RRRR/MM/DD:')
+    t2 = input('Podaj drugą datę w formacie RRRR/MM/DD:')
+
+    y1 = t1[0:4]
+    m1 = t1[5:7]
+    d1 = t1[8:10]
+
+
+    t1 = datetime.date(int(t1[0:4]),int(t1[5:7]),int(t1[8:10]))
+    t2 = datetime.date(int(t2[0:4]),int(t2[5:7]),int(t2[8:10]))
+
+    dt = t1-t2
+    print(dt)
