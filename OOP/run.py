5b8dd304ffaddf1a5167728c87e55c7f2b4b51d1
olusK <kozak.aleksandra96@gmail.com>
2023-01-08 23:14:55 +0100

    delete feature

--- OOP/run.py
+++ OOP/run.py
@@ -8,6 +8,8 @@ def main():
         [2] - dodaj wizytówkę \r\n
         [3] - wyświetl notatki \r\n
         [4] - wyświetl wizytówki \r\n
+        [5] - usuń notatkę \r\n
+        [6] - usuń wizytówkę \r\n
         [0] - zamknij organizer '''
                        )
         switcher(button)
@@ -25,6 +27,12 @@ def switcher(button):
     elif(button =="4"):
         visitCard = Organiser()
         visitCard.showVisitingCards()
+    elif(button == "5"):
+        note = Organiser()
+        note.deleteNote()
+    elif(button =="6"):
+        visitCard = Organiser()
+        visitCard.deleteVisitingCard()
     else:
         sys.exit()
 
